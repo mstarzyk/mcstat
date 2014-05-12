b468b6942f7773033e68acf724e18522c9d8f774
Maciek Starzyk <mstarzyk@gmail.com>
2014-05-13 01:13:57 +0200

    Version 0.2

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(name='mcstat',
-      version='0.2.dev',
+      version='0.2',
       description='Multicast statistics',
       author='Maciej Starzyk',
       author_email='mstarzyk@gmail.com',
-      url='',
-      # tests_require=['pytest'],
+      url='https://github.com/mstarzyk/mcstat',
       packages=['mcstat', 'mcstat.tests'],
       entry_points={
           'console_scripts': [
